bf26a4198655f9adef9a3cf3505c47563c4ab372
Semikgn <semi.merzi05@gmail.com>
2025-11-15 00:23:33 +0300

    Sidebar update.2

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ st.set_page_config(layout="wide")
 st.title("BIST Veri Takipçisi")
 st.write("Bu proje, GitHub Actions ile toplanan BIST verilerini gösterir.")
 
+# Veri çek
 try:
     csv_dosyasi_adi = 'gunluk_veriler.csv'
     df = pd.read_csv(csv_dosyasi_adi)
@@ -18,9 +19,54 @@ except pd.errors.EmptyDataError:
     st.warning("Veritabanı ('gunluk_veriler.csv') şu anda boş. Lütfen V1 otomasyonunun çalışmasını bekleyin.")
     st.stop()
 
-st.sidebar.header("Hisse Listesi")
+# Session State
 hisse_listesi = sorted(df['Hisse Kodu'].unique())
-secilen_hisse = st.sidebar.radio("Grafiği Görüntüle:", hisse_listesi)
+
+if 'secilen_hisse' not in st.session_state:
+    st.session_state.secilen_hisse = hisse_listesi[0]
+
+st.sidebar.header("Hisse Listesi")
+
+# Hafızayı güncelleyecek yardımcı fonksiyon
+def set_hisse(hisse_kodu):
+    st.session_state.secilen_hisse = hisse_kodu
+
+# Her hisse için "Hücre" oluştur
+for hisse in hisse_listesi:
+    # O hisseye ait veriyi al ve tarihe göre sırala
+    hisse_data = df[df['Hisse Kodu'] == hisse].sort_values('Date')
+    
+    # Fiyat ve değişim hesaplaması yap
+    if len(hisse_data) < 2:
+        # Eğer yeterli veri yoksa (1 günden az)x
+        last_price = hisse_data.iloc[-1]['Close'] if len(hisse_data) == 1 else 0
+        delta_str = None # Değişim hesaplanamaz
+        change_pct = 0
+    else:
+        last_price = hisse_data.iloc[-1]['Close']
+        prev_price = hisse_data.iloc[-2]['Close']
+        change_pct = ((last_price - prev_price) / prev_price) * 100
+        delta_str = f"{change_pct:.2f}%"
+
+    # Difference in price increase/decrease
+    st.sidebar.metric(
+        label=hisse, 
+        value=f"{last_price:.2f} TL", # Son Fiyat
+        delta=delta_str 
+    )
+    
+    # Click button -> update state
+    st.sidebar.button(
+        "Grafiği Görüntüle", 
+        key=f"btn_{hisse}", 
+        on_click=set_hisse, 
+        args=(hisse,), 
+        use_container_width=True 
+    )    
+    st.sidebar.divider()
+
+# Grafik ve Tablo
+secilen_hisse_kodu = st.session_state.secilen_hisse
 
 st.header(f"{secilen_hisse} Fiyat Grafiği")
 
